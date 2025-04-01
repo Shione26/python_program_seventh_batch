78095be3b6a5be292e056bdf92e2bebe3d741195
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:04:35 +0800

    count the characters then increment the variable count then print count

--- print_without_using_count.py
+++ print_without_using_count.py
@@ -7,5 +7,8 @@ character = input("Enter character to count: ")
 # initialize a variable to store the count
 count = 0
 # check each character in text if it matches the character to count
-# if yes, increment variable
-# print output
+for char in text:
+    if char == character:
+        count += 1  # if yes, increment variable
+# print output  
+print(count)
