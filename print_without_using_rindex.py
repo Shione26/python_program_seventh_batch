075bea17a391b12b2ade495df32757ac81ed1210
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:28:56 +0800

    find the last occurence of the word then print output

--- print_without_using_rindex.py
+++ print_without_using_rindex.py
@@ -5,5 +5,8 @@ text = input("Enter text: ")
 # ask the user what word to find
 word = input("Enter word to find: ")
 # using rfind(), find the last occurrence of word in text
-# if not found, print -1
-# otherwise, print position
+result = text.rfind(word)
+if result == -1:
+    print("-1")     # If not found, print -1
+else:
+    print(result)   # Otherwise, print position
