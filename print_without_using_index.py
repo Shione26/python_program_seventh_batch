8d8dcbd55ece7d4ea2ef8c564107503036558b5e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:16:02 +0800

    pseudocode

---
+++ print_without_using_index.py
@@ -0,0 +1,7 @@
+# Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# ask the user for input
+# ask the user what word to find
+# using find(), find the first occurence of word in text
+# if not found, print -1
+# otherwise, print position
