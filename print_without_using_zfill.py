0231054def5fedf035690f6df81430fc596d6f11
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:31:50 +0800

    ask for inputs

--- print_without_using_zfill.py
+++ print_without_using_zfill.py
@@ -1,7 +1,9 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # ask the user for input
+string = str(input("Enter string: "))
 # ask the user for the total number of characters
+total_char = int(input("Enter total number of characters: "))
 # subtract the length of the string to the total number of characters to get the zeroes
 # concatenate the zeroes and the string
 # print output
