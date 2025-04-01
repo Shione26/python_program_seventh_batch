1eb91fc7417297242afd68694aaf99b246fe5820
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:44:06 +0800

    ask for input

--- print_without_using_upper.py
+++ print_without_using_upper.py
@@ -1,6 +1,7 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # create a list of uppercase letters
 # create a list of lowercase letters
 # initialize an empty string to store the output
