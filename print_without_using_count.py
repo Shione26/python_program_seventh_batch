9ef018b9bf65b61b08cff0959cb23c80e6f4bc98
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:02:57 +0800

    ask for input and initialized a variable

--- print_without_using_count.py
+++ print_without_using_count.py
@@ -1,8 +1,11 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # ask the user what character to count
+character = input("Enter character to count: ")
 # initialize a variable to store the count
+count = 0
 # check each character in text if it matches the character to count
 # if yes, increment variable
 # print output
