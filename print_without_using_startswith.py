c7d78288f5736813a02fbeb75fbce28d3b6da1dd
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:53:15 +0800

    ask for inputs

--- print_without_using_startswith.py
+++ print_without_using_startswith.py
@@ -1,7 +1,9 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # ask the user the prefix
+prefix = input("Enter prefix: ")
 # check if the prefix is at the beginning
 # if yes, print True
 # if no, print False
