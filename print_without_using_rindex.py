634b34a18c1699133668668ee3cf8b64537068e8
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:26:01 +0800

    pseudocode

---
+++ print_without_using_rindex.py
@@ -0,0 +1,7 @@
+# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+
+# ask the user for input
+# ask the user what word to find
+# using rfind(), find the last occurrence of word in text
+# if not found, print -1
+# otherwise, print position
