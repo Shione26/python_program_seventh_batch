c957d76ad2a848a9c46ef3ee2b5a6dfbf058a25e
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:16:55 +0800

    ask for inputs

--- print_without_using_index.py
+++ print_without_using_index.py
@@ -1,7 +1,9 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # ask the user what word to find
+word = input("Enter word to find: ")
 # using find(), find the first occurence of word in text
 # if not found, print -1
 # otherwise, print position
