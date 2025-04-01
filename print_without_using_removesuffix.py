a04838c9e30dc930f6cf8a826c23f8c8df9a329d
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:36:48 +0800

    pseudocode

---
+++ print_without_using_removesuffix.py
@@ -0,0 +1,7 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+# ask the user for input
+# ask the user for suffix
+# check if the text ends with the suffix
+# if yes, remove the prefix using slicing
+# print output
