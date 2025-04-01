eb009a6d7bedb90f7afd5ac6b22ca83b5f72ab58
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:30:20 +0800

    ask for input

--- print_without_using_rstrip.py
+++ print_without_using_rstrip.py
@@ -1,6 +1,7 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
-# ask the user for input 
+# ask the user for input
+text = str(input("Enter text: ")) 
 # inspect each character backwards from the end if it is a non-space character
 # if found a non-space, break
 # print output
