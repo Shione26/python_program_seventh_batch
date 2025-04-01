0185ab7b43fb93a27ed85651c9d4f359237ff396
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:28:53 +0800

    pseudocode

---
+++ print_without_using_rstrip.py
@@ -0,0 +1,6 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# ask the user for input 
+# inspect each character backwards from the end if it is a non-space character
+# if found a non-space, break
+# print output
