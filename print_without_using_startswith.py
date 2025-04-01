c31981aed9796d4ba7448f3f3200b664d535fda6
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:52:34 +0800

    pseudocode

---
+++ print_without_using_startswith.py
@@ -0,0 +1,7 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# ask the user for input
+# ask the user the prefix
+# check if the prefix is at the beginning
+# if yes, print True
+# if no, print False
