d1a9c363640e697f999ac906b04f797efab90797
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:27:28 +0800

    ask for inputs

--- print_without_using_rindex.py
+++ print_without_using_rindex.py
@@ -1,7 +1,9 @@
 # Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # ask the user what word to find
+word = input("Enter word to find: ")
 # using rfind(), find the last occurrence of word in text
 # if not found, print -1
 # otherwise, print position
