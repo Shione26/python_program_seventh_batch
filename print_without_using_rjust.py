b0c04265f83e69817e9447bb797aeea860634d0b
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:58:02 +0800

    ask for inputs

--- print_without_using_rjust.py
+++ print_without_using_rjust.py
@@ -1,7 +1,9 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.'
 
 # ask the user for input
+text = str(input("Enter text: "))
 # ask the user for the width
+width = int(input("Enter width: "))
 # compute the number of spaces to be filled at the beginning
 # concatenate the spaces and the text
 # print the result
