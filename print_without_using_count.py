0f4a1c39ff4214985178dd6735e53d4e898573cb
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 01:01:59 +0800

    pseudocode

---
+++ print_without_using_count.py
@@ -0,0 +1,8 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# ask the user for input
+# ask the user what character to count
+# initialize a variable to store the count
+# check each character in text if it matches the character to count
+# if yes, increment variable
+# print output
