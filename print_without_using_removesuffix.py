51112b15ecedb00b50888fff1fc8de10e2d41244
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-04-02 00:37:30 +0800

    ask for inputs

--- print_without_using_removesuffix.py
+++ print_without_using_removesuffix.py
@@ -1,7 +1,9 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # ask the user for input
+text = input("Enter text: ")
 # ask the user for suffix
+suffix = input("Enter suffix: ")
 # check if the text ends with the suffix
 # if yes, remove the prefix using slicing
 # print output
